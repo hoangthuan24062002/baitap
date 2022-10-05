5d37c01c059eb1bdef1698eabae6189f9ac80360
hoangthuan24062002 <74901479+hoangthuan24062002@users.noreply.github.com>
2022-10-05 20:37:19 +0700

    Add files via upload

---
+++ Kiemtra.java
@@ -0,0 +1,11 @@
+public class Kiemtra {
+    public static void main(String[] args) {
+        IEmployee employee1 = new PartTimeEmployee("Thuan", 45000, 7);
+        System.out.println("Name: " + employee1.getName());
+        System.out.println("Salary per day: " + employee1.calculateSalary());
+
+        IEmployee employee2 = new FullTimeEmployee("Nam", 65000);
+        System.out.println("Name: " + employee2.getName());
+        System.out.println("Salary per day: " + employee2.calculateSalary());
+    }
+}
